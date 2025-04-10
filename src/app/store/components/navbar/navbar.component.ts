595d3ab6cfb19df82463a9b15504054773da08ac
William Mosquera <wjmmk3@gmail.com>
2025-04-10 09:59:34 -0500

    fix: navbar-menu updated

--- src/app/store/components/navbar/navbar.component.ts
+++ src/app/store/components/navbar/navbar.component.ts
@@ -2,9 +2,8 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
-  standalone: true,
-  selector: 'front-navbar',
-  imports: [RouterLink, RouterLinkActive],
-  templateUrl: './navbar.component.html'
+    selector: 'front-navbar',
+    imports: [RouterLink, RouterLinkActive],
+    templateUrl: './navbar.component.html'
 })
 export class NavbarComponent { }
