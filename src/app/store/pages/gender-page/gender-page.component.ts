f82d39134fd35f9087e76c0837e0f69bf2e2a34d
William Mosquera <wjmmk3@gmail.com>
2025-04-10 10:09:49 -0500

    feat: gender-component created

--- src/app/store/pages/gender-page/gender-page.component.ts
+++ src/app/store/pages/gender-page/gender-page.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  standalone: true,
-  selector: 'app-gender-page',
-  imports: [],
-  templateUrl: './gender-page.component.html'
+    selector: 'app-gender-page',
+    imports: [],
+    templateUrl: './gender-page.component.html'
 })
 export class GenderPageComponent { }
