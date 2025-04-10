a1780c471d6cb00e76012c7a88d300dd34689703
William Mosquera <wjmmk3@gmail.com>
2025-04-10 10:04:05 -0500

    feat: not-found component added

--- src/app/store/pages/not-found-page/not-found-page.component.ts
+++ src/app/store/pages/not-found-page/not-found-page.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  standalone: true,
-  selector: 'app-not-found-page',
-  imports: [],
-  templateUrl: './not-found-page.component.html'
+    selector: 'app-not-found-page',
+    imports: [],
+    templateUrl: './not-found-page.component.html'
 })
 export class NotFoundPageComponent { }
