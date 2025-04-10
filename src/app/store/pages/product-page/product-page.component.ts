49c53afcafc5ed9235b9918b3180312001e161e8
William Mosquera <wjmmk3@gmail.com>
2025-04-10 10:07:36 -0500

    feat: page to display products advanced

--- src/app/store/pages/product-page/product-page.component.ts
+++ src/app/store/pages/product-page/product-page.component.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
+import { ProductsService } from '@products/services/products.service';
 
 @Component({
-  standalone: true,
-  selector: 'app-product-page',
-  imports: [],
-  templateUrl: './product-page.component.html'
+    selector: 'app-product-page',
+    imports: [],
+    templateUrl: './product-page.component.html'
 })
-export class ProductPageComponent { }
+export class ProductPageComponent {
+  activatedRoute = inject(ActivatedRoute);
+  productsService = inject(ProductsService);
+
+  productIdOrSlug: string = this.activatedRoute.snapshot.params['idSlug'];
+
+  productResource = rxResource({
+    request: () => ({ idSlug: this.productIdOrSlug }),
+    loader: ({ request }) => {
+      return this.productsService.getProductByIdOrSlug(request.idSlug);
+    }
+  });
+}
