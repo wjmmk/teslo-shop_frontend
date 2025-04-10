72135c9b095ebb1372ee9c1beb64a8c78e14009b
William Mosquera <wjmmk3@gmail.com>
2025-04-10 10:08:59 -0500

    feat: Layout advanced

--- src/app/store/layout/store-front-layout/store-front-layout.component.ts
+++ src/app/store/layout/store-front-layout/store-front-layout.component.ts
@@ -3,9 +3,8 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from '@store/components/navbar/navbar.component';
 
 @Component({
-  standalone: true,
-  selector: 'app-store-front-layout',
-  imports: [RouterOutlet, NavbarComponent],
-  templateUrl: './store-front-layout.component.html',
+    selector: 'app-store-front-layout',
+    imports: [RouterOutlet, NavbarComponent],
+    templateUrl: './store-front-layout.component.html'
 })
 export class StoreFrontLayoutComponent { }
