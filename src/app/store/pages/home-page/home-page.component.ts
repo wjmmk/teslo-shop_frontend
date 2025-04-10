234c39a69fc2d0b92baf19c41d5d6bb70ac9a617
William Mosquera <wjmmk3@gmail.com>
2025-04-10 10:03:14 -0500

    fix: Initial view updated

--- src/app/store/pages/home-page/home-page.component.ts
+++ src/app/store/pages/home-page/home-page.component.ts
@@ -1,23 +1,22 @@
 import { Component, inject } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
 import { ProductCardComponent } from '@products/components/product-card/product-card.component';
 import { ProductsService } from '@products/services/products.service';
-//import { rxResource } from '@shared/utils/rx-resource';
 
 
 @Component({
-  standalone: true,
-  selector: 'app-home-page',
-  imports: [ProductCardComponent],
-  templateUrl: './home-page.component.html'
+    selector: 'app-home-page',
+    imports: [ProductCardComponent],
+    templateUrl: './home-page.component.html'
 })
 export class HomePageComponent {
 
   productsService = inject(ProductsService);
 
- /*  productsResource = rxResource({
+  productsResource = rxResource({
     request: () => ({}),
-    loader: ({ request}) => { this.productsService.getAllProducts() },
-    error: () => ({ error: 'Error loading products' }),
-    loading: () => ({ loading: true }),
-  }) */
+    loader: ({ request }) => {
+      return this.productsService.getAllProducts({});
+    }
+  });
 }
