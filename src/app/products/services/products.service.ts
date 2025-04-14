cf1a261072a71006fd8730b04665ce553a016df9
William Mosquera <wjmmk3@gmail.com>
2025-04-14 09:54:51 -0500

    fix: updating number

--- src/app/products/services/products.service.ts
+++ src/app/products/services/products.service.ts
@@ -16,7 +16,7 @@ export class ProductsService {
   private productCache = new Map<string, Product>();
 
   getAllProducts(options: Options):Observable<ProductsResponse> {
-    const { limit = 9, offset = 0, gender = '' } = options;
+    const { limit = 12, offset = 0, gender = '' } = options;
 
     const key = `${limit}-${offset}-${gender}`; // Esto me crea una llave para ser utilizada en el caché-
 
