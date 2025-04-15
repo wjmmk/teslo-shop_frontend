56c448894fe8118caab2e62483a141d1db2c8b80
William Mosquera <wjmmk3@gmail.com>
2025-04-15 15:05:52 -0500

    fix: store-layout component updated

--- src/app/store/layout/store-front-layout/store-front-layout.component.ts
+++ src/app/store/layout/store-front-layout/store-front-layout.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from '@store/components/navbar/navbar.component';
+import { FooterComponent } from "../../components/footer/footer.component";
+
 
 @Component({
     selector: 'app-store-front-layout',
-    imports: [RouterOutlet, NavbarComponent],
+    imports: [RouterOutlet, NavbarComponent, FooterComponent],
     templateUrl: './store-front-layout.component.html'
 })
 export class StoreFrontLayoutComponent { }
