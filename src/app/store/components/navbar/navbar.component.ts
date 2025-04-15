a8e2acc47213cc3dc8566a56919a37209b00c35b
William Mosquera <wjmmk3@gmail.com>
2025-04-15 15:09:07 -0500

    fix: navbar component cleared

--- src/app/store/components/navbar/navbar.component.ts
+++ src/app/store/components/navbar/navbar.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
-import { FooterComponent } from "../footer/footer.component";
+
 
 @Component({
     selector: 'front-navbar',
