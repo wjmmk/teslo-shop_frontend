89234deb13916e0b2044d4fa6726594bb15d9f75
William Mosquera <wjmmk3@gmail.com>
2025-04-24 11:31:57 -0500

    feat: global interceptor implemented

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,12 +2,21 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { loggingInterceptor } from '@shared/interceptors/logging.interceptor';
+import { authInterceptor } from '@auth/interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(withFetch()), // permite usar fetch en las peticiones.
+    provideHttpClient(  // permite usar fetch en las peticiones.
+      withFetch(),
+      withInterceptors([
+        // interceptor para logear las peticiones http
+        loggingInterceptor,
+        authInterceptor
+      ])
+    ),
   ]
 };
