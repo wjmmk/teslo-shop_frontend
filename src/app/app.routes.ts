2006c6e3f96e3049ff607d8b39611736707711d0
William Mosquera <wjmmk3@gmail.com>
2025-04-24 11:34:26 -0500

    feat: not-auth-guard implemented

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
+import { NotAuthenticatedGuard } from '@auth/guards/not-authenticated.guard';
 
 export const routes: Routes = [
   {
     path: 'auth',
-    loadChildren: () => import('./auth/auth.routes')
-    //TODO: Guards
+    loadChildren: () => import('./auth/auth.routes'),
+    canMatch: [NotAuthenticatedGuard]
   },
   {
     path: '',
