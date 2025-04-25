441cf2f16b9732266b5cea53407412ad49a3a969
William Mosquera <wjmmk3@gmail.com>
2025-04-25 08:40:36 -0500

    feat: utils file updated

---
+++ src/app/Utils/form-utils.ts
@@ -0,0 +1,43 @@
+import { FormArray, FormGroup, ValidationErrors } from "@angular/forms";
+
+
+export class FormUtils {
+
+  static isValidField(form: FormGroup, fieldName: string) {
+    return (
+      !!form.controls[fieldName].errors && form.controls[fieldName].touched
+    )
+  }
+
+  static getTextError(errors: ValidationErrors) {
+    for( const key of Object.keys(errors)) {
+      switch(key){
+        case 'required':
+          return 'Este es un campo requerido' // null
+        case 'minlength':
+          return `Mínimo de ${errors['minlength'].requiredLength} caracteres.`
+        case 'min':
+          return `Valor mínimo de ${errors['min'].min}`
+      }
+    }
+    return null;
+  }
+
+  static getFieldError(form: FormGroup, fieldName: string): string | null {
+    if(!form.controls[fieldName]) return null;
+    const errors = form.controls[fieldName].errors ?? {}
+    return this.getTextError(errors);
+  }
+
+  static isValidFieldInArray(formArray: FormArray, index: number) {
+    return (
+      formArray.controls[index].errors && formArray.controls[index].touched
+    )
+  }
+
+  static getFieldErrorInArray(formArray: FormArray, index: number): string | null {
+    if (formArray.controls.length === 0) return null;
+    const errors = formArray.controls[index].errors ?? {}
+    return this.getTextError(errors);
+  }
+}
