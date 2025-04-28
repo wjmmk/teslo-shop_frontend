88a45bc026208b1bc7e29cf4206757abcc522f77
William Mosquera <wjmmk3@gmail.com>
2025-04-28 13:48:16 -0500

    feat: Update of app.config.ts with HashLocation()

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
@@ -9,7 +9,7 @@ import { authInterceptor } from '@auth/interceptors/auth.interceptor';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideRouter(routes, withHashLocation()),
     provideHttpClient(  // permite realizar peticiones asyncronas hacia el servidor.
       withFetch(),
       withInterceptors([
