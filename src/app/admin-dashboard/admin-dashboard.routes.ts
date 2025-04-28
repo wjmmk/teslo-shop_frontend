7cfd5ac95f5b0127c4cb97d0c197607d0ca3616a
William Mosquera <wjmmk3@gmail.com>
2025-04-28 13:36:14 -0500

    feat: routes dashboard added

---
+++ src/app/admin-dashboard/admin-dashboard.routes.ts
@@ -0,0 +1,29 @@
+import { Routes } from "@angular/router";
+import { AdminDashboardLayoutComponent } from "./layouts/admin-dashboard-layout/admin-dashboard-layout.component";
+import { ProductsAdminPageComponent } from "./pages/products-admin-page/products-admin-page.component";
+import { ProductAdminPageComponent } from "./pages/product-admin-page/product-admin-page.component";
+
+
+const adminDashboardRoutes: Routes = [
+  {
+    path: '',
+    component: AdminDashboardLayoutComponent,
+    children: [
+      {
+        path: 'products',
+        component: ProductsAdminPageComponent
+      },
+      {
+        path: 'products/:id',
+        component: ProductAdminPageComponent
+      },
+      {
+        path: '**',
+        redirectTo: 'products'
+      }
+    ]
+  }
+];
+
+
+export default adminDashboardRoutes;
