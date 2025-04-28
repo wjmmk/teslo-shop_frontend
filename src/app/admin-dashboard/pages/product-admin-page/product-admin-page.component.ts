cab808464a28ed9f3b9ae510faf8eeddc9d8825a
William Mosquera <wjmmk3@gmail.com>
2025-04-28 13:40:22 -0500

    feat: Admin-dashboard-page created

---
+++ src/app/admin-dashboard/pages/product-admin-page/product-admin-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'product-admin-page',
+  imports: [],
+  templateUrl: './product-admin-page.component.html'
+})
+export class ProductAdminPageComponent { }
