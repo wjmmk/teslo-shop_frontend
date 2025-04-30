9020d9cf353e97697d167b40869fc504d2fe278b
William Mosquera <wjmmk3@gmail.com>
2025-04-30 10:54:32 -0500

    fix: navbar-component movil implemented.

--- src/app/store/components/navbar/navbar.component.ts
+++ src/app/store/components/navbar/navbar.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { AuthService } from '@auth/services/auth.service';
@@ -5,9 +6,26 @@ import { AuthService } from '@auth/services/auth.service';
 
 @Component({
     selector: 'front-navbar',
-    imports: [RouterLink, RouterLinkActive],
+    imports: [RouterLink, RouterLinkActive, CommonModule],
     templateUrl: './navbar.component.html'
 })
 export class NavbarComponent {
   authServices = inject(AuthService);
+  isMenuOpen: boolean = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+    // Como ocultar los botones login y registrarse
+    const loginButton = document.getElementById('login');
+    const registerButton = document.getElementById('register');
+    if (this.isMenuOpen) {
+      loginButton?.classList.add('hidden');
+      registerButton?.classList.add('hidden');
+    }
+    else {
+      loginButton?.classList.remove('hidden');
+      registerButton?.classList.remove('hidden');
+    }
+  }
+
 }
