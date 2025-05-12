34dd999bf5078f26215ed4e70418296d9df1d9b0
William Mosquera <wjmmk3@gmail.com>
2025-05-12 15:48:19 -0500

    fix: Implementation of the skeleton functionality done.

--- src/app/store/pages/home-page/home-page.component.ts
+++ src/app/store/pages/home-page/home-page.component.ts
@@ -1,24 +1,50 @@
-import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
 import { rxResource } from '@angular/core/rxjs-interop';
+import { ProductCardSkeletonComponent } from '@products/components/product-card-skeleton/product-card-skeleton.component';
 import { ProductCardComponent } from '@products/components/product-card/product-card.component';
 import { ProductsService } from '@products/services/products.service';
 import { PaginationComponent } from '@shared/components/pagination/pagination.component';
 import { PaginationService } from '@shared/components/pagination/pagination.service';
+import { catchError, finalize, switchMap, throwError, timeout, timer } from 'rxjs';
 
 @Component({
-    selector: 'app-home-page',
-    imports: [ProductCardComponent, PaginationComponent],
+    selector: 'home-page',
+    imports: [ProductCardComponent, PaginationComponent, ProductCardSkeletonComponent, CommonModule],
     templateUrl: './home-page.component.html'
 })
-export class HomePageComponent {
-  productsService = inject(ProductsService);
+export class HomePageComponent implements OnInit {
+  isLoading = true;
+  hasError = false;
 
+  productsService = inject(ProductsService);
   paginationService = inject(PaginationService)
+
+  ngOnInit() {
+    this.productsService.getAllProducts({ offset: 9 });
+  }
 
   productsResource = rxResource({
     request: () => ({ page: this.paginationService.currentPage() - 1 }),
     loader: ({ request }) => {
-      return this.productsService.getAllProducts({ offset: request.page * 9 }); // Esto me permite decidir cuantos productos puedo saltar.
+      this.isLoading = true;
+      this.hasError = false;
+
+      return timer(10000) // le damos un pequeño delay para efecto visual suave
+        .pipe(
+          switchMap(() =>
+            this.productsService.getAllProducts({ offset: request.page * 9 }).pipe(
+              timeout(30000), // ⏰ espera máxima de 30 segundos
+              catchError((error) => {
+                this.hasError = true;
+                return throwError(() => error);
+              }),
+              finalize(() => {
+                this.isLoading = false;
+              })
+            )
+          )
+        );
     }
   });
 }
