968d63e66a347ac60ee5c24926755daafaadc6b6
William Mosquera <wjmmk3@gmail.com>
2025-05-14 16:22:04 -0500

    feat: Is-admin guard implementation done.

--- src/app/admin-dashboard/admin-dashboard.routes.ts
+++ src/app/admin-dashboard/admin-dashboard.routes.ts
@@ -2,12 +2,15 @@ import { Routes } from "@angular/router";
 import { AdminDashboardLayoutComponent } from "./layouts/admin-dashboard-layout/admin-dashboard-layout.component";
 import { ProductsAdminPageComponent } from "./pages/products-admin-page/products-admin-page.component";
 import { ProductAdminPageComponent } from "./pages/product-admin-page/product-admin-page.component";
+import { IsAdminGuard } from "./guards/is-admin.guard";
 
 
 const adminDashboardRoutes: Routes = [
   {
     path: '',
+    title: 'Admin Dashboard',
     component: AdminDashboardLayoutComponent,
+    canMatch: [IsAdminGuard],
     children: [
       {
         path: 'products',
