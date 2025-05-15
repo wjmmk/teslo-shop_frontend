29b2e3c44087e37d1db1c2eaf9142319b08e088a
William Mosquera <wjmmk3@gmail.com>
2025-05-15 11:34:10 -0500

    feat: Product-details component added.

--- src/app/admin-dashboard/pages/product-admin-page/product-admin-page.component.ts
+++ src/app/admin-dashboard/pages/product-admin-page/product-admin-page.component.ts
@@ -3,10 +3,12 @@ import { rxResource, toSignal } from '@angular/core/rxjs-interop';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ProductsService } from '@products/services/products.service';
 import { map } from 'rxjs';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+
 
 @Component({
   selector: 'product-admin-page',
-  imports: [],
+  imports: [ProductDetailsComponent],
   templateUrl: './product-admin-page.component.html'
 })
 export class ProductAdminPageComponent {
