3aa438c0f3abb47cb20bb614125785f899e46971
William Mosquera <wjmmk3@gmail.com>
2025-05-15 00:38:08 -0500

    feat: Products-admin.page component implemented.

--- src/app/admin-dashboard/pages/products-admin-page/products-admin-page.component.ts
+++ src/app/admin-dashboard/pages/products-admin-page/products-admin-page.component.ts
@@ -1,8 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { ProductTableComponent } from "../product-table/product-table.component";
+import { rxResource } from '@angular/core/rxjs-interop';
+import { ProductsService } from '@products/services/products.service';
+import { PaginationService } from '@shared/components/pagination/pagination.service';
+import { PaginationComponent } from "../../../shared/components/pagination/pagination.component";
 
 @Component({
   selector: 'products-admin-page',
-  imports: [],
+  imports: [ProductTableComponent, PaginationComponent],
   templateUrl: './products-admin-page.component.html'
 })
-export class ProductsAdminPageComponent { }
+export class ProductsAdminPageComponent {
+
+  productsService = inject(ProductsService);
+  paginationService = inject(PaginationService);
+
+  productsPerPage = signal(10);
+
+  readonly productsResource = rxResource({
+    request: () => ({
+      page: this.paginationService.currentPage() - 1,
+      limit: this.productsPerPage()
+    }),
+    loader: ({ request }) => {
+      return this.productsService.getAllProducts({
+        offset: request.page * 9,
+        limit: request.limit
+      })
+    }
+  });
+
+}
+
+
