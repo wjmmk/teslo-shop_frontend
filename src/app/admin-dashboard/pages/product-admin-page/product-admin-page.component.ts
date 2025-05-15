bbb31fdebd3b589903cd6f83c9fb01f02526beae
William Mosquera <wjmmk3@gmail.com>
2025-05-15 11:07:37 -0500

    feat: Implementation of product-admin-page initialized.

--- src/app/admin-dashboard/pages/product-admin-page/product-admin-page.component.ts
+++ src/app/admin-dashboard/pages/product-admin-page/product-admin-page.component.ts
@@ -1,8 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, effect, inject } from '@angular/core';
+import { rxResource, toSignal } from '@angular/core/rxjs-interop';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProductsService } from '@products/services/products.service';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'product-admin-page',
   imports: [],
   templateUrl: './product-admin-page.component.html'
 })
-export class ProductAdminPageComponent { }
+export class ProductAdminPageComponent {
+  activatedRoute = inject(ActivatedRoute);
+  productsService = inject(ProductsService);
+  router = inject(Router);
+
+  productId = toSignal(
+    this.activatedRoute.params.pipe(
+      map(params => params['id'])
+    )
+  );
+
+  productResource = rxResource({
+    request: () => ({ id: this.productId() }),
+    loader: ({ request }) => {
+      return this.productsService.getProductById(request.id);
+    }
+  });
+
+  // Sacar de la ruta si el id no existe.
+  redirectEffect = effect(() => {
+    if(this.productResource.error()) {
+      this.router.navigate(['/admin/products']);
+      return;
+    }
+  })
+}
