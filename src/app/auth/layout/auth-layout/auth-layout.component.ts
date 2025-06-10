b9492a4c8eab0ab10de8a7b99abb03ad6ffcbf08
William Mosquera <wjmmk3@gmail.com>
2025-06-10 09:42:28 -0500

    fix: Structured auth-layout component to display the navbar menu and footer.

--- src/app/auth/layout/auth-layout/auth-layout.component.ts
+++ src/app/auth/layout/auth-layout/auth-layout.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "../../../store/components/navbar/navbar.component";
+import { FooterComponent } from "../../../store/components/footer/footer.component";
 
 @Component({
   selector: 'auth-layout',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './auth-layout.component.html',
 })
 export class AuthLayoutComponent { }
