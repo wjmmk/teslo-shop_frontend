e4a4f8f262d3a1f75ffd1043ea8a588a22b08452
William Mosquera <wjmmk3@gmail.com>
2025-06-17 23:56:02 -0500

    feat: Temporary implementation of the calculateVisualRating() method to obtain a rating.

--- src/app/products/components/product-card/product-card.component.ts
+++ src/app/products/components/product-card/product-card.component.ts
@@ -1,5 +1,5 @@
 import { SlicePipe } from '@angular/common';
-import { Component, computed, input } from '@angular/core';
+import { Component, computed, input, SimpleChanges } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { environment } from '@environments/environment.development';
 import { Product } from '@products/interfaces/product.interface';
@@ -14,8 +14,43 @@ import { TransLatePipe } from '@shared/pipes/translate.pipe';
 export class ProductCardComponent {
   product = input.required<Product>();
   baseUrl = environment.baseUrl;
+  productsArray: Product[] = [];
+  visualRating: number = 0;
+
+  // Si el product viene como Input, es buena práctica recalcular si cambia
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['product']) {
+      this.calculateVisualRating();
+    }
+  }
+
+  // Metodo para calcular el rating visual basado en el stock, esta es una implementacion algo absurda pero funcional.
+  private calculateVisualRating(): void {
+    if (this.product && typeof this.product().stock === 'number') {
+      const stockAsString = this.product().stock.toString();
+      if (stockAsString.length > 0) {
+        // Tomamos el primer dígito y lo convertimos a número
+        let firstDigit = parseInt(stockAsString.charAt(0), 10);
+
+        // Aseguramos que el rating esté entre 1 y 5
+        // Si el primer dígito es 0, lo ponemos a 1 (o el valor mínimo que quieras)
+        if (firstDigit === 0) {
+          firstDigit = 1;
+        }
+        // Si el primer dígito es mayor que 5, lo limitamos a 5
+        if (firstDigit > 5) {
+          firstDigit = 5;
+        }
+        this.visualRating = firstDigit;
+      }
+    }
+  }
 
   imageUrl = computed( () => {
     return `${this.baseUrl}/files/product/${this.product().images[0]}`;
   })
+
+
+  addToCart() {}
+
 }
