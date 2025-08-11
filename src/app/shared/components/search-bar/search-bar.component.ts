1f9874b5f9dbb3d19e85d131dd2004d96927d7f6
William Mosquera <wjmmk3@gmail.com>
2025-08-11 17:26:03 -0500

    feat: Search-bar component logic enabled.

--- src/app/shared/components/search-bar/search-bar.component.ts
+++ src/app/shared/components/search-bar/search-bar.component.ts
@@ -1,63 +1,90 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, linkedSignal, signal } from '@angular/core';
 import { rxResource } from '@angular/core/rxjs-interop';
 import { Router } from '@angular/router';
 import { Product } from '@products/interfaces/product.interface';
 import { ProductsService } from '@products/services/products.service';
+import { TransLatePipe } from '@shared/pipes/translate.pipe';
 import { debounceTime, distinctUntilChanged, Subscription } from 'rxjs';
 
 @Component({
   selector: 'search-bar',
-  imports: [],
+  imports: [TransLatePipe],
   templateUrl: './search-bar.component.html',
-  styleUrl: './search-bar.component.css'
+  styleUrl: './search-bar.component.css',
+  providers: [TransLatePipe]
 })
 export class SearchBarComponent {
   searchTerm = signal<string>('');
   private searchSubscription?: Subscription;
+  private inputFocused = signal(false);
+  /* products = signal<Product[]>([]); */
 
   private readonly _router = inject(Router);
   private readonly _productsService = inject(ProductsService);
 
   constructor() { }
 
-  products = signal<Product[]>([]);
+  products = linkedSignal<Product[]>(() => this.filterProducts.value()?.products ?? ([] as Product[]));
 
-  onSearch(event: any) {
-    const term = event.target.value;
+  filterProducts = rxResource({
+    request: this.searchTerm,
+    loader: () => this._productsService.getAllProductsToFilter(this.searchTerm())
+  });
 
-    console.log('Search term: ', term);
+  //De este metodo debo obtener el Id y pasarselo al API response para que busque el producto especifico.
+  onSearchInput(event: Event): void {
+    const term = (event.target as HTMLInputElement).value;
 
     if (this.searchSubscription) {
       this.searchSubscription.unsubscribe();
     }
 
-    this.searchSubscription = this._productsService.getAllProducts(term)
+    if (term.length < 2) {
+      this.products.set([]);
+      return;
+    }
+
+    this.searchSubscription = this._productsService
+      .getAllProductsToFilter(term)
       .pipe(
         debounceTime(300),
         distinctUntilChanged()
       )
-      .subscribe((products: any) => {
-        this.products.set(products);
+      .subscribe(response => {
+        this.products.set(response.products);
       });
+
+    this._clearSearch();
   }
 
-  goToDetails(id: string) {
-    this.products.set([]);
+  goToDetails(id: string): void {
     this._router.navigate(['/product', id]);
+    this._clearSearch();
   }
 
-  ngOnDestroy() {
+  ngOnDestroy(): void {
     this.searchSubscription?.unsubscribe();
   }
 
-  filterProducts = rxResource({
-    request: this.searchTerm,
-    loader: () => this._productsService.getProductById(this.searchTerm())
-  });
-
   //Limpio el termino a buscar dentro de la barra de busqueda
-  private _clearQuery(): void {
+  private _clearSearch(): void {
     this.searchTerm.set('');
   }
 
+  //Referencia para el filtrado de productos.
+
+  isInputFocused() {
+    return this.inputFocused();
+  }
+
+  onFocus() {
+    this.inputFocused.set(true);
+  }
+
+  onBlur() {
+    // Pequeño delay para permitir que el click en los resultados funcione
+    setTimeout(() => {
+      this.inputFocused.set(false);
+    }, 200);
+  }
 }
