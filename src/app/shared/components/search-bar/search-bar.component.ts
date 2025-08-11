964e4f0c7ed48b3101f37dafab8657fdf0fa809c
William Mosquera <wjmmk3@gmail.com>
2025-08-11 09:47:33 -0500

    feat: Basic implementation of search-component.

--- src/app/shared/components/search-bar/search-bar.component.ts
+++ src/app/shared/components/search-bar/search-bar.component.ts
@@ -1,4 +1,9 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { Router } from '@angular/router';
+import { Product } from '@products/interfaces/product.interface';
+import { ProductsService } from '@products/services/products.service';
+import { debounceTime, distinctUntilChanged, Subscription } from 'rxjs';
 
 @Component({
   selector: 'search-bar',
@@ -7,10 +12,52 @@ import { Component, EventEmitter, Output } from '@angular/core';
   styleUrl: './search-bar.component.css'
 })
 export class SearchBarComponent {
-  @Output() search = new EventEmitter<string>();
+  searchTerm = signal<string>('');
+  private searchSubscription?: Subscription;
 
-  onSearch(event: Event) {
-    const value = (event.target as HTMLInputElement).value;
-    this.search.emit(value);
+  private readonly _router = inject(Router);
+  private readonly _productsService = inject(ProductsService);
+
+  constructor() { }
+
+  products = signal<Product[]>([]);
+
+  onSearch(event: any) {
+    const term = event.target.value;
+
+    console.log('Search term: ', term);
+
+    if (this.searchSubscription) {
+      this.searchSubscription.unsubscribe();
+    }
+
+    this.searchSubscription = this._productsService.getAllProducts(term)
+      .pipe(
+        debounceTime(300),
+        distinctUntilChanged()
+      )
+      .subscribe((products: any) => {
+        this.products.set(products);
+      });
   }
+
+  goToDetails(id: string) {
+    this.products.set([]);
+    this._router.navigate(['/product', id]);
+  }
+
+  ngOnDestroy() {
+    this.searchSubscription?.unsubscribe();
+  }
+
+  filterProducts = rxResource({
+    request: this.searchTerm,
+    loader: () => this._productsService.getProductById(this.searchTerm())
+  });
+
+  //Limpio el termino a buscar dentro de la barra de busqueda
+  private _clearQuery(): void {
+    this.searchTerm.set('');
+  }
+
 }
