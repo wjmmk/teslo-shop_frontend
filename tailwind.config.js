1e4bb5a34c7ec82dfe53288ee8e5970e97b68820
William Mosquera <wjmmk3@gmail.com>
2025-08-14 10:50:23 -0500

    feat: Great animations to register button done.

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,12 +10,23 @@ module.exports = {
     extend: {
       animation:{
         fadeIn:'fadeIn 0.5s ease-in-out',
+        lightRing: 'lightRing 3s linear infinite',
+        rotateRing: 'rotateRing 3s linear infinite',
       },
       keyframes:{
         fadeIn: {
           '0%': { opacity: 0 },
           '100%': { opacity: 1 },
-        }
+        },
+        lightRing: {
+          '0%': { backgroundPosition: '0% 50%' },
+          '50%': { backgroundPosition: '100% 50%' },
+          '100%': { backgroundPosition: '0% 50%' },
+        },
+        rotateRing: {
+          '0%': { transform: 'rotate(0deg)' },
+          '100%': { transform: 'rotate(360deg)' },
+        },
       }
     },
   },
