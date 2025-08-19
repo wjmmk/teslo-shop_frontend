803892ca50a5fdec2974890aafbe97c77bcee47b
William Mosquera <wjmmk3@gmail.com>
2025-08-19 17:11:46 -0500

    feat: Shoppingcart general implementation implemented.

--- src/app/store/pages/product-page/product-page.component.ts
+++ src/app/store/pages/product-page/product-page.component.ts
@@ -1,18 +1,24 @@
 import { Component, inject } from '@angular/core';
 import { rxResource } from '@angular/core/rxjs-interop';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { ProductCarouselComponent } from '@products/components/product-carousel/product-carousel.component';
 import { ProductsService } from '@products/services/products.service';
 import { TransLatePipe } from "../../../shared/pipes/translate.pipe";
+import { ProductsCartService } from '@products/services/products-cart.service';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
     selector: 'app-product-page',
-    imports: [ProductCarouselComponent, TransLatePipe],
+    imports: [ProductCarouselComponent, TransLatePipe, CurrencyPipe],
     templateUrl: './product-page.component.html'
 })
 export class ProductPageComponent {
   activatedRoute = inject(ActivatedRoute);
   productsService = inject(ProductsService);
+  productsCartService = inject(ProductsCartService);
+  rating: number = 4.5;
+
+  constructor(private router: Router) { }
 
   productIdOrSlug: string = this.activatedRoute.snapshot.params['idSlug'];
 
@@ -23,7 +29,8 @@ export class ProductPageComponent {
     }
   });
 
-
-  addToCart() {}
-  rating: number = 4.5;
+  addToCart() {
+    this.productsCartService.addProduct(this.productResource.value()!);
+    this.router.navigate(['/']);
+  }
 }
