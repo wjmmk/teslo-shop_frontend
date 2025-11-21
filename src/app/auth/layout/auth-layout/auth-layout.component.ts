d125e218d620269d4045501c14849164943c29f8
William Mosquera <wjmmk3@gmail.com>
2025-11-20 21:26:18 -0500

    fix: add routerLink import and update auth layout template

--- src/app/auth/layout/auth-layout/auth-layout.component.ts
+++ src/app/auth/layout/auth-layout/auth-layout.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "../../../store/components/navbar/navbar.component";
 import { FooterComponent } from "../../../store/components/footer/footer.component";
 
 @Component({
   selector: 'auth-layout',
-  imports: [RouterOutlet, NavbarComponent, FooterComponent],
+  imports: [RouterOutlet, RouterLink, NavbarComponent, FooterComponent],
   templateUrl: './auth-layout.component.html',
 })
 export class AuthLayoutComponent { }
